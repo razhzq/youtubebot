8ca47ee184c328e90a2f623da8c9ae03a094af08
razhzq <74318704+razhzq@users.noreply.github.com>
2021-01-26 22:22:32 +0800

    Update ytbot.py

--- ytbot.py
+++ ytbot.py
@@ -6,6 +6,7 @@ Created on Tue Jan 26 17:45:07 2021
 """
 
 
+
 from selenium import webdriver
 from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 from time import sleep
@@ -13,10 +14,10 @@ from time import sleep
 
 
 
-req_proxy = RequestProxy() #you may get different number of proxy when  you run this at each time
-proxies = req_proxy.get_proxy_list() #this will create proxy list
+req_proxy = RequestProxy() 
+proxies = req_proxy.get_proxy_list() 
 
-for i in range(proxies[900]):
+for i in range(len(proxies)):
  proxy = proxies[i].get_address()
  webdriver.DesiredCapabilities.CHROME['proxy']={
     "httpProxy":proxy,
@@ -28,7 +29,13 @@ for i in range(proxies[900]):
  }
 
  driver = webdriver.Chrome(executable_path=r'C:\Users\Loga\Desktop\AIRUS TECH\Sneaker Bot\chromedriver.exe')
- driver.get('https://www.youtube.com/watch?v=BO6goZQONvY&t=384s')
- driver.maximize_window()
- driver.refresh()
+ try:
+  driver.get('https://www.youtube.com/watch?v=BO6goZQONvY&t=384s')
+  driver.maximize_window()
+  driver.refresh()
+  driver.sleep(120)
+  driver.quit()
+ except:
+     driver.quit()
+     pass
  
